4b14bb4be022c7b7a96259cf49dddde79e261c3b
jasonkuyper <60154156+jasonkuyper@users.noreply.github.com>
2022-11-30 15:07:42 -0500

    Finished creating tests for existing features. Also created tests for new update feature

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -71,7 +71,7 @@ describe('Route integration', () => {
           .send(testUserInfo)
           .expect(200)
           .then(async (res) => {
-            console.log('res.body', res.body);
+            // console.log('res.body', res.body);
             const { first_name, last_name, email } = res.body;
             expect({ first_name, last_name, email }).toEqual(testObj);
           });
@@ -87,9 +87,169 @@ describe('Route integration', () => {
       it("Successfully sends user's job application info", async () => {
         const res = await request(server).get(`/users/${userID}`);
         // expect('Content-Type', /application\/json/);
-        console.log('res.body', res.body);
+        // console.log('res.body', res.body);
         const testData = await db.query(testQuery, testParams);
         expect(res.body).toEqual(testData.rows);
+      });
+    });
+  });
+
+  describe('/jobs', () => {
+    const userID = 23;
+    let jobID;
+
+    const testJob = {
+      job_role: 'Test Role',
+      company_name: 'Test Inc.',
+      phone: '1-800-TEST-NOW',
+      email: 'test123@test.com',
+      contact_name: 'Test Name',
+      job_link: 'www.test.com/test',
+      status: 'testing',
+      user_id: userID,
+    };
+
+    const newTestStatus = { newStatus: 'still testing' };
+
+    let newTestJob;
+
+    const updatedTestJob = {
+      job_role: 'Updated Test Role',
+      company_name: 'NewTest Inc.',
+      phone: '1-888-NEW-TEST',
+      email: 'updatedtest@test.com',
+      contact_name: 'Newtest Name',
+      job_link: 'www.newtest.com/newtest',
+      status: 'new instance of testing',
+      user_id: userID,
+    };
+
+    describe('POST', () => {
+      it('Creates a new job list by inserting job info into SQL database', () => {
+        return request(server)
+          .post('/jobs')
+          .send(testJob)
+          .expect(200)
+          .then(async (res) => {
+            // console.log('res.body', res.body);
+            const {
+              job_role,
+              company_name,
+              phone,
+              email,
+              contact_name,
+              job_link,
+              status,
+              user_id,
+              job_id,
+            } = res.body;
+            jobID = job_id;
+            expect({
+              job_role,
+              company_name,
+              phone,
+              email,
+              contact_name,
+              job_link,
+              status,
+              user_id,
+            }).toEqual(testJob);
+          });
+      });
+    });
+
+    describe('PATCH', () => {
+      it('Updates existing job list from SQL database', () => {
+        return request(server)
+          .patch(`/jobs/${jobID}`)
+          .send(newTestStatus)
+          .expect(200)
+          .then(async (res) => {
+            console.log('patch res.body', res.body);
+            const {
+              job_role,
+              company_name,
+              phone,
+              email,
+              contact_name,
+              job_link,
+              status,
+              user_id,
+            } = res.body;
+            newTestJob = { ...testJob, status: newTestStatus.newStatus };
+            console.log('newTestJob', newTestJob);
+            expect({
+              job_role,
+              company_name,
+              phone,
+              email,
+              contact_name,
+              job_link,
+              status,
+              user_id,
+            }).toEqual(newTestJob);
+          });
+      });
+    });
+
+    // describe('PATCH', () => {
+    //   it('Updates all fields of job application record', () => {
+    //     return request(server)
+    //       .patch(`/jobs/${jobID}`)
+    //       .expect(200)
+    //       .then(async (res) => {
+    //         const {
+    //           job_role,
+    //           company_name,
+    //           phone,
+    //           email,
+    //           contact_name,
+    //           job_link,
+    //           status,
+    //           user_id,
+    //         } = res.body;
+    //         expect({
+    //           job_role,
+    //           company_name,
+    //           phone,
+    //           email,
+    //           contact_name,
+    //           job_link,
+    //           status,
+    //           user_id,
+    //         }).toEqual(updatedTestJob);
+    //       });
+    //   });
+    // });
+
+    describe('DELETE', () => {
+      it('Deletes existing job list from SQL database', () => {
+        return request(server)
+          .delete(`/jobs/${jobID}`)
+          .expect(200)
+          .then(async (res) => {
+            // console.log('res.body', res.body);
+            const {
+              job_role,
+              company_name,
+              phone,
+              email,
+              contact_name,
+              job_link,
+              status,
+              user_id,
+            } = res.body;
+            expect({
+              job_role,
+              company_name,
+              phone,
+              email,
+              contact_name,
+              job_link,
+              status,
+              user_id,
+            }).toEqual(newTestJob);
+          });
       });
     });
   });
